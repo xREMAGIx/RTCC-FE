1dab951c6a3355bf1cfbaa70bb91914b9db09d26
Remagi <tramdo2806@gmail.com>
2023-03-23 22:49:41 +0700

    :sparkles: feat(welcome-register-ui): add welcome, register page UI

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -7,48 +7,72 @@ import Heading from 'components/atoms/Heading';
 import Input from 'components/atoms/Input';
 import Text from 'components/atoms/Text';
 import Container from 'components/organisms/Container';
+import { EMAIL_REGEX } from 'utils/constants';
 
-export type LoginFormTypes = {
+type RegisterFormTypes = {
+  email: string;
   username: string;
   password: string;
 };
 
-const Login: React.FC = () => {
+const Register: React.FC = () => {
   //* Hooks
   const navigate = useNavigate();
 
   //* React hook form
-  const method = useForm<LoginFormTypes>({
+  const method = useForm<RegisterFormTypes>({
     defaultValues: {
+      email: '',
       username: '',
       password: '',
     },
   });
 
   //* Functions
-  const customSubmit = (data: LoginFormTypes) => {
+  const customSubmit = (data: RegisterFormTypes) => {
     console.log(data);
   };
 
   return (
     <Container>
-      <div className="p-login">
-        <div className="p-login_content">
-          <div className="p-login_title">
-            <Heading modifiers={['32x42', 'eerieBlack', 'center', '700']} content="Login" />
+      <div className="p-register">
+        <div className="p-register_content">
+          <div className="p-register_title">
+            <Heading modifiers={['32x42', 'eerieBlack', 'center', '700']} content="Register" />
           </div>
-          <div className="p-login_description u-mt-16">
+          <div className="p-register_description u-mt-16">
             <Text
               modifiers={['16x24', 'stormcloud', 'center', '500']}
-              content="Login to create room and access to many more features."
+              content="Register to create room and access to many more features."
             />
           </div>
-          <div className="p-login_form u-mt-32">
+          <div className="p-register_form u-mt-32">
             <FormProvider {...method}>
               <form
                 noValidate
               >
-                <div className="p-login_form_username u-mt-24">
+                <div className="p-register_form_email u-mt-24">
+                  <Controller
+                    name="email"
+                    rules={{
+                      required: 'Email is required',
+                      pattern: {
+                        value: new RegExp(EMAIL_REGEX),
+                        message: 'Email must have valid format',
+                      }
+                    }}
+                    render={({ field, fieldState }) => (
+                      <Input
+                        {...field}
+                        label="Email"
+                        placeholder="Email"
+                        id="email"
+                        error={fieldState.error?.message}
+                      />
+                    )}
+                  />
+                </div>
+                <div className="p-register_form_username u-mt-16">
                   <Controller
                     name="username"
                     rules={{
@@ -65,7 +89,7 @@ const Login: React.FC = () => {
                     )}
                   />
                 </div>
-                <div className="p-login_form_password u-mt-16">
+                <div className="p-register_form_password u-mt-16">
                   <Controller
                     name="password"
                     rules={{
@@ -83,19 +107,19 @@ const Login: React.FC = () => {
                     )}
                   />
                 </div>
-                <div className="p-login_form_submit u-mt-24">
+                <div className="p-register_form_submit u-mt-24">
                   <Button
                     modifiers={['primary', 'lg']}
                     type="submit"
                     loading={false}
                     onClick={method.handleSubmit(customSubmit)}
                   >
-                    <Text modifiers={['16x24', '600', 'white', 'center']} content="Login" />
+                    <Text modifiers={['16x24', '600', 'white', 'center']} content="Register" />
                   </Button>
                 </div>
               </form>
             </FormProvider>
-            <div className="p-login_back_btn u-mt-24">
+            <div className="p-register_back_btn u-mt-24">
               <Button
                 modifiers={['outlineGreen', 'lg']}
                 type="button"
@@ -111,4 +135,4 @@ const Login: React.FC = () => {
   );
 };
 
-export default Login;
+export default Register;
