bcec9867c6dfafeb6f15948ca4be8327c10b34da
Remagi <tramdo2806@gmail.com>
2023-03-28 00:37:26 +0700

    :sparkles: feat(api-login): implement api login & add confirm password for register form

--- src/services/user/types.ts
+++ src/services/user/types.ts
@@ -1,3 +1,10 @@
+export interface UserData {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
 export interface RegisterUserParams {
   username: string;
   email: string;
@@ -5,5 +12,11 @@ export interface RegisterUserParams {
 }
 
 export interface LoginUserParams {
+  username: string;
+  password: string;
+}
 
+export interface LoginUserData {
+  user: UserData;
+  token: string;
 }
